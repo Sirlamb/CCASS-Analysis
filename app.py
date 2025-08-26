b3fb6cf8ecca96714c651306909bfbcfad92a798
unknown <Sirlamb>
2025-08-27 00:11:54 +0800

    First commit of CCASS

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, send_file
+from flask import Flask, render_template, request
+import json
+import sharehold
+from datetime import datetime, timedelta, timezone
+import re
+
+today = format(datetime.now(timezone.utc) + timedelta(hours=8), '%Y%m%d')
+
+app = Flask(__name__, template_folder='template')
+
+@app.route("/")
+def searchbar():
+    """
+    Displays the homepage and search bar of the search engine.
+    """
+    # history = json.loads(request.cookies.get('history', default="{}"))
+    # return render_template("index.html",HISTORY=history)
+    return render_template("index.html")
+
+
+@app.route("/search", methods=['POST'])
+def submit_search():
+    """
+    Performs the search and displays the results in the webpage.
+    """
+
+    # Get query from search bar
+    stock = request.form.get('searchbar')
+
+    startDate = request.form.get('start-date')
+    startDate = sharehold.convert_date_format(startDate) 
+
+    endDate = request.form.get('end-date')
+    endDate = sharehold.convert_date_format(endDate) 
+
+    stock = sharehold.validate_arg_code(stock)
+    startDate = sharehold.validate_arg_date(startDate)
+    endDate = sharehold.validate_arg_date(endDate)
+    
+    start_json = sharehold.searchsdw(today, startDate, stock)
+    end_json = sharehold.searched(today, endDate, stock)
+
+    return "here"
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ sharehold.py
@@ -0,0 +1,288 @@
+import sys
+import re
+import requests
+import urllib.parse
+import json
+import pandas as pd
+import argparse
+import textwrap
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta, timezone
+
+def validate_arg_code(code):
+    if code.isdigit() and len(code) <= 5:
+        return code
+    else:
+        print('Invalid Stock Code')
+        raise ValueError('Invalid Stock Code')
+    
+def convert_date_format(date):
+    date_obj = datetime.strptime(date, '%Y-%m-%d')
+    date_formatted = date_obj.strftime('%Y/%m/%d')
+    return date_formatted
+
+def validate_arg_date(date):
+    pattern_str = r'^\d{4}/\d{2}/\d{2}$'    # "date: YYYY/MM/DD"
+    try:
+        if re.match(pattern_str, date) and bool(datetime.strptime(date, '%Y/%m/%d')):
+            return date
+        else:
+            raise ValueError('Invalid Shareholding Date')
+    except ValueError:
+        print('Invalid Shareholding Date')
+        raise
+
+def searchsdw(today, txtShareholdingDate, txtstockCode):
+    try:
+
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36', 'Content-Type': 'application/x-www-form-urlencoded'}
+        url_address = 'https://www3.hkexnews.hk/sdw/search/searchsdw_c.aspx'
+        data = '__EVENTTARGET=btnSearch&__EVENTARGUMENT=&today=' + today + '&sortBy=shareholding&sortDirection=desc&alertMsg=&txtShareholdingDate=' + txtShareholdingDate + '&txtStockCode=' + txtStockCode + '&txtStockName=&txtParticipantID=&txtParticipantName=&txtSelPartID='
+        url = requests.request('POST', url_address, data=data, headers=headers)
+        source = BeautifulSoup(url.text, 'html.parser')
+        res = source.find_all('tbody')
+        data = []
+        
+        for row in res[0]("tr"):
+            items = {}
+            items["id"] = row.find('td',attrs={'class','col-participant-id'}).find('div',attrs={'class','mobile-list-body'}).text.strip()
+            items["name"] = row.find('td',attrs={'class','col-participant-name'}).find('div',attrs={'class','mobile-list-body'}).text.strip()
+            items["shareholding"] = row.find('td',attrs={'class','col-shareholding'}).find('div',attrs={'class','mobile-list-body'}).text.strip()
+            items["shareholding-percent"] = row.find('td',attrs={'class','col-shareholding-percent'}).find('div',attrs={'class','mobile-list-body'}).text.strip()
+            items["is_matched"] = 'N'
+            data.append(items)
+
+        return data
+    
+    except:
+        print("HKEX server issue. Please try again later")
+        raise
+
+
+if __name__ == "__main__":
+
+    today = format(datetime.now(timezone.utc) + timedelta(hours=8), '%Y%m%d')
+    code = '941' 
+    txtStockCode = "{:05d}".format(int(code))
+
+    txtShareholdingDateStart = urllib.parse.quote("2024/11/01", safe='')
+    txtShareholdingDateEnd = urllib.parse.quote("2025/01/02", safe='')
+
+    json_start = searchsdw(today, txtShareholdingDateStart, txtStockCode)
+    json_end = searchsdw(today, txtShareholdingDateEnd, txtStockCode)
+
+    # Megre JSONs
+    data = []
+    table = []
+    index = []
+    for entry_json_start in json_start:
+        for entry_json_end in json_end:
+            if entry_json_start["id"] == entry_json_end["id"] and entry_json_start["name"] == entry_json_end["name"]:
+                entry_json_start["is_matched"], entry_json_end["is_matched"]  = 'Y', 'Y'
+                shareholding_start = int(entry_json_start["shareholding"].replace(',',''))
+                shareholding_end = int(entry_json_end["shareholding"].replace(',',''))
+                shareholding_change = shareholding_end - shareholding_start
+                shareholding_change_percent = shareholding_change / shareholding_start * 100
+
+                items = {}
+                items["id"] = entry_json_start["id"] 
+                items["name"] = entry_json_start["name"]
+                items["start"] = shareholding_start
+                items["start-display"] = entry_json_start["shareholding"] + ' (' + entry_json_start["shareholding-percent"] + ')'
+                items["end"] = shareholding_end
+                items["end-display"] = entry_json_end["shareholding"] + ' (' + entry_json_end["shareholding-percent"] + ')'
+                items["change"] = shareholding_change
+                items["change-percent"] = shareholding_change_percent
+                items["change-display"] = f"{shareholding_change:,}" + ' (' + f'{round(shareholding_change_percent,2):.2f}' + '%)'
+                data.append(items)
+
+    for entry_json_start in json_start:
+        if entry_json_start["is_matched"] == 'N':
+            shareholding_start = int(entry_json_start["shareholding"].replace(',',''))
+            shareholding_end = 0
+            shareholding_change = shareholding_end - shareholding_start
+            shareholding_change_percent = -100
+
+            items = {}
+            items["id"] = entry_json_start["id"] 
+            items["name"] = entry_json_start["name"]
+            items["start"] = shareholding_start
+            items["start-display"] = entry_json_start["shareholding"] + ' (' + entry_json_start["shareholding-percent"] + ')'
+            items["end"] = shareholding_end
+            items["end-display"] = '0 (0.00%)'
+            items["change"] = shareholding_change
+            items["change-percent"] = shareholding_change_percent
+            items["change-display"] = f"{shareholding_change:,}" + ' (-100.00%)'
+            data.append(items)
+
+    for entry_json_end in json_end:
+        if entry_json_end["is_matched"] == 'N':
+            shareholding_start = 0
+            shareholding_end = int(entry_json_end["shareholding"].replace(',',''))
+            shareholding_change = shareholding_end - shareholding_start
+            shareholding_change_percent = 100
+            items = {}
+            items["id"] = entry_json_end["id"] 
+            items["name"] = entry_json_end["name"] 
+            items["start"] = shareholding_start
+            items["start-display"] = '0 (0.00%)'
+            items["end"] = shareholding_end
+            items["end-display"] = entry_json_end["shareholding"] + ' (' + entry_json_end["shareholding-percent"] + ')'
+            items["change"] = shareholding_change
+            items["change-percent"] = shareholding_change_percent
+            items["change-display"] = f"{shareholding_change:,}" + ' (100.00%)'
+            data.append(items)
+
+    print(data)
+
+
+
+# if __name__ == "__main__":
+
+#     try:
+#         # Prepare the args
+#         parser=argparse.ArgumentParser(
+#             formatter_class=argparse.RawDescriptionHelpFormatter,
+#             description=textwrap.dedent('''\
+#                 You can search and compare two dates of CCASS shareholding information from HKEX source.
+#                 example: python CCASS.py -c 941 -s 2023/10/01 -e 2024/01/02 --lang c --sort change
+                                        
+#                 you can also request for instructions by: python CCASS.py -h 
+
+#                 Please provide following informaiton:
+#                 ------------------------------------------------------------------------------------------------------------
+#                 -c CODE               Stock Code (example: 1)
+#                 -s START              Shareholding Start Date (example: 2024/12/30)
+#                 -e END                Shareholding End Date (example: 2024/12/31)
+#                 --lang                Display Language (default: c)
+#                                       Options:
+#                                         e: English
+#                                         c: Chinese
+#                 --sort                Sort by value (default: change)
+#                                       Options:
+#                                         id: Participant Id
+#                                         name: Participant Name
+#                                         start: Shareholding Start
+#                                         end: Shareholding End
+#                                         change: Shareholding Change
+#                                         change-percent: Shareholding Change Percent
+#                 ------------------------------------------------------------------------------------------------------------
+#                 '''),
+#             epilog=textwrap.dedent('''\
+#                 License
+#                 ------------------------------------------------------------------------------------------------------------
+#                 MIT - a permissive free software license originating at the Massachusetts 
+#                 Institute of Technology (MIT), it puts only very limited restriction on 
+#                 reuse and has, therefore, an excellent license compatibility. It permits 
+#                 reuse within proprietary software provided that all copies of the licensed 
+#                 software include a copy of the MIT License terms and the copyright notice.
+#                 ''')
+#         )
+
+#         parser.add_argument('-c', dest='code', required=True, type=validate_arg_code)
+#         parser.add_argument('-s', dest='start', required=True, type=validate_arg_start)
+#         parser.add_argument('-e', dest='end', required=True, type=validate_arg_end)
+#         parser.add_argument('--lang', dest='lang', choices=['e','c'], default='c',)
+#         parser.add_argument('--sort', dest='sort', choices=['id','name','start','end','change','change-percent'], default='change')
+#         # parser.add_argument('--id', )
+#         args=parser.parse_args()
+
+#         txtStockCode = "{:05d}".format(int(args.code))
+#         txtShareholdingDateStart = urllib.parse.quote(args.start, safe='')
+#         txtShareholdingDateEnd = urllib.parse.quote(args.end, safe='')
+#         today = format(datetime.now(timezone.utc) + timedelta(hours=8), '%Y%m%d')
+
+#         # Call HTTP request to get data from HKEX
+#         json_start = searchsdw(today, txtShareholdingDateStart, txtStockCode, args.lang)
+#         json_end = searchsdw(today, txtShareholdingDateEnd, txtStockCode, args.lang)
+        
+#         # Megre JSONs
+#         data = []
+#         table = []
+#         index = []
+#         for entry_json_start in json_start:
+#             for entry_json_end in json_end:
+#                 if entry_json_start["id"] == entry_json_end["id"] and entry_json_start["name"] == entry_json_end["name"]:
+#                     entry_json_start["is_matched"] = 'Y'
+#                     entry_json_end["is_matched"] = 'Y'
+#                     shareholding_start = int(entry_json_start["shareholding"].replace(',',''))
+#                     shareholding_end = int(entry_json_end["shareholding"].replace(',',''))
+#                     shareholding_change = shareholding_end - shareholding_start
+#                     shareholding_change_percent = shareholding_change / shareholding_start * 100
+#                     items = {}
+#                     items["id"] = entry_json_start["id"] 
+#                     items["name"] = entry_json_start["name"]
+#                     items["start"] = shareholding_start
+#                     items["start-display"] = entry_json_start["shareholding"] + ' (' + entry_json_start["shareholding-percent"] + ')'
+#                     items["end"] = shareholding_end
+#                     items["end-display"] = entry_json_end["shareholding"] + ' (' + entry_json_end["shareholding-percent"] + ')'
+#                     items["change"] = shareholding_change
+#                     items["change-percent"] = shareholding_change_percent
+#                     items["change-display"] = f"{shareholding_change:,}" + ' (' + f'{round(shareholding_change_percent,2):.2f}' + '%)'
+#                     data.append(items)
+
+#         for entry_json_start in json_start:
+#             if entry_json_start["is_matched"] == 'N':
+#                 shareholding_start = int(entry_json_start["shareholding"].replace(',',''))
+#                 shareholding_end = 0
+#                 shareholding_change = shareholding_end - shareholding_start
+#                 shareholding_change_percent = -100
+#                 items = {}
+#                 items["id"] = entry_json_start["id"] 
+#                 items["name"] = entry_json_start["name"]
+#                 items["start"] = shareholding_start
+#                 items["start-display"] = entry_json_start["shareholding"] + ' (' + entry_json_start["shareholding-percent"] + ')'
+#                 items["end"] = shareholding_end
+#                 items["end-display"] = '0 (0.00%)'
+#                 items["change"] = shareholding_change
+#                 items["change-percent"] = shareholding_change_percent
+#                 items["change-display"] = f"{shareholding_change:,}" + ' (-100.00%)'
+#                 data.append(items)
+
+#         for entry_json_end in json_end:
+#             if entry_json_end["is_matched"] == 'N':
+#                 shareholding_start = 0
+#                 shareholding_end = int(entry_json_end["shareholding"].replace(',',''))
+#                 shareholding_change = shareholding_end - shareholding_start
+#                 shareholding_change_percent = 100
+#                 items = {}
+#                 items["id"] = entry_json_end["id"] 
+#                 items["name"] = entry_json_end["name"] 
+#                 items["start"] = shareholding_start
+#                 items["start-display"] = '0 (0.00%)'
+#                 items["end"] = shareholding_end
+#                 items["end-display"] = entry_json_end["shareholding"] + ' (' + entry_json_end["shareholding-percent"] + ')'
+#                 items["change"] = shareholding_change
+#                 items["change-percent"] = shareholding_change_percent
+#                 items["change-display"] = f"{shareholding_change:,}" + ' (100.00%)'
+#                 data.append(items)
+
+#         # Sort result JSON
+#         data.sort(key=lambda k: k[args.sort], reverse=True)
+
+#         # Prepare display table
+#         for json_data_sort_item in data:
+#             table.append([json_data_sort_item["start-display"],json_data_sort_item["end-display"],json_data_sort_item["change-display"]])
+#             index.append(json_data_sort_item["id"] + ' - ' + json_data_sort_item["name"] if len(json_data_sort_item["id"]) > 0 else json_data_sort_item["name"])
+
+#         pd.set_option('display.max_rows', None)
+#         pd.set_option('display.max_columns', None)
+#         pd.set_option('display.width', 2000)
+#         pd.set_option('display.max_colwidth', None)
+#         pd.set_option("display.unicode.east_asian_width", True)
+        
+#         if args.lang == 'c' :
+#             df = pd.DataFrame(table, columns = [args.start, args.end, '持股量 (%)'], index=index)
+#         elif args.lang == 'e':
+#             df = pd.DataFrame(table, columns = [args.start, args.end, 'Shareholding (%)'], index=index)
+        
+#         print(df)
+#         pd.reset_option('display.max_rows')
+#         pd.reset_option('display.max_columns')
+#         pd.reset_option('display.width')
+#         pd.reset_option('display.max_colwidth')
+
+
+#     except Exception as error:
+
+#         print("exception error")
